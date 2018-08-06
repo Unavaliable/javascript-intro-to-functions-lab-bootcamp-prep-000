c53d038c8e4fd9eb70b6568ae2c7517b980935fb
jahkarih23@gmail.com <jahkarih23@gmail.com>
2018-08-06 15:58:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string;
+}
+
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string;
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+var toUpperCase = sayHiToGrandma;
+if(
+  sayHiToGrandma === toUpperCase
+  )console.log("YES INDEED!");
+else{
+  console.log("I can't hear you!")
+}
